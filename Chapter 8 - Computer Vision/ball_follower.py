a643c177c499e76eb788a692004e0578ef053859
Matt Timmons-Brown (Raspberry Pi Guy) <theraspberrypiguy@gmail.com>
2020-06-16 18:21:23 +0100

    Fix typo

--- Chapter 8 - Computer Vision/ball_follower.py
+++ Chapter 8 - Computer Vision/ball_follower.py
@@ -31,13 +31,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 	color_mask = cv2.inRange(hsv, lower_color, upper_color)
 
-	image2, countours, hierarchy = cv2.findContours(color_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	image2, contours, hierarchy = cv2.findContours(color_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 	object_area = 0
 	object_x = 0
 	object_y = 0
 
-	for contour in countours:
+	for contour in contours:
 		x, y, width, height = cv2.boundingRect(contour)
 		found_area = width * height
 		center_x = x + (width / 2)
